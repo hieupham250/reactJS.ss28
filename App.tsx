cf7e82534c987356a5a4722f75955ad6932f3165
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-05-05 10:21:18 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+// import B1 from "./components/B1";
+// import B2 from "./components/B2";
+// import B3 from "./components/B3";
+// import B4 from "./components/B4";
+// const fullName: string = "Phạm Trung Hiếu";
+
+// import B5 from "./components/B5";
+// const product = {
+//   id: 1,
+//   name: "Tôi thấy hoa vàng trên cỏ xanh",
+//   price: 10000,
+//   quantity: 5,
+// };
+
+// import B6 from "./components/B6";
+// const users = [
+//   { id: 1, name: "Phạm Trung Hiếu", address: "Hà Nội", email: "abc@gmail.com" },
+//   { id: 2, name: "Cao Thị Mỹ Tâm", address: "Đà Nẵng", email: "xyz@gmail.com" },
+//   { id: 3, name: "Nguyễn Thanh Sang", address: "HCM", email: "wer@gamil.com" },
+// ];
+
+// import B7 from "./components/B7";
+// const posts = [
+//   { id: 1, title: "Post 1", content: "Content 1", author: "Author 1" },
+//   { id: 2, title: "Post 2", content: "Content 2", author: "Author 2" },
+//   { id: 3, title: "Post 3", content: "Content 3", author: "Author 3" },
+// ];
+
+// import B8 from "./components/B8";
+// const products = [
+//   { id: 1, name: "Tôi thấy hoa vàng trên cỏ xanh", price: 200000, quantity: 5 },
+//   { id: 1, name: "Đắc nhân tâm", price: 100000, quantity: 10 },
+//   { id: 1, name: "Dế mèn phiêu lưu ký", price: 120000, quantity: 15 },
+// ];
+
+// import B9 from "./components/B9";
+
+import B10 from "./components/B10";
+
+export default function App() {
+  return (
+    <div>
+      {/* <B1></B1> */}
+      {/* <B2></B2> */}
+      {/* <B3></B3> */}
+      {/* <B4 name={fullName}></B4> */}
+      {/* <B5 product={product}></B5> */}
+      {/* <B6 users={users}></B6> */}
+      {/* <B7 posts={posts}></B7> */}
+      {/* <B8 products={products}></B8> */}
+      {/* <B9></B9> */}
+      <B10></B10>
+    </div>
+  );
+}
